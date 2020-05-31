4e1e3e8e6e86a6b4a48f1cada3d977f2f2a5922f
Sam Titarsolej <s.titarsolej@gmail.com>
2020-05-31 14:43:54 +0200

    TweetLoader update

---
+++ LoadData.py
@@ -0,0 +1,54 @@
+import datetime
+import tweepy
+import csv
+import urllib.request
+
+from pathlib import Path
+from TwitterConfig import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
+def get_tweets(screen_name, save_csv=True):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    tweets = []
+    tweet_count = 0
+
+    statuses = tweepy.Cursor(api.user_timeline, id=screen_name).items()
+    for status in statuses:
+        if 'media' in status.entities.keys():
+            if tweet_count % 50 == 0:
+                print('{} tweets downloaded'.format(tweet_count))
+            tweet_count += 1
+
+            try:
+                rating = int(status.text.split('/10')[0][-2:])
+                tweet_data = [status.id_str, rating,
+                            status.entities['media'][0]['media_url']]
+                tweets.append(tweet_data)
+            except:
+                continue
+    
+    save_in_dir(tweets)
+
+    if save_csv:
+        with open(f'data/tweets.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['id', 'rating', 'media'])
+            writer.writerows(tweets)
+    return tweets
+
+def save_in_dir(tweets):
+    # Create class dirs
+    for i in range(11):
+        Path('data/dataset/{}'.format(i)).mkdir(parents=True, exist_ok=True)
+
+    for i, tweet in enumerate(tweets):
+        if i % 100 == 0:
+            print('{}/{} images downloaded'.format(i, len(tweets)))
+
+        urllib.request.urlretrieve(tweet[2],
+            'data/dataset/{}/{}.jpg'.format(tweet[1], i))
+
+if __name__ == '__main__':
+    get_tweets('ratemyskyperoom')
